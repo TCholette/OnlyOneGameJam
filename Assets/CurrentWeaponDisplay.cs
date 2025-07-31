0a93ade2e8a8b4c215ef141b13f07b337f04d002
TCholette <128748862+TCholette@users.noreply.github.com>
2025-07-30 23:32:34 -0400

    :)

--- Assets/CurrentWeaponDisplay.cs
+++ Assets/CurrentWeaponDisplay.cs
@@ -6,23 +6,5 @@ public class CurrentWeaponDisplay : MonoBehaviour
     [SerializeField] private GameObject _spear;
     [SerializeField] private GameObject _shield;
 
-    public void SelectWeapon(Weapon weapon) {
-        switch (weapon) {
-            case Weapon.Sword: 
-                _sword.SetActive(true);
-                _spear.SetActive(false);
-                _shield.SetActive(false);
-                break;
-            case Weapon.Spear:
-                _sword.SetActive(false);
-                _spear.SetActive(true);
-                _shield.SetActive(false);
-                break;
-            case Weapon.Shield:
-                _sword.SetActive(false);
-                _spear.SetActive(false);
-                _shield.SetActive(true);
-                break;
-        }
-    }
+
 }
--- Assets/Scripts/Player/WeaponSelect.cs
+++ Assets/Scripts/Player/WeaponSelect.cs
@@ -7,7 +7,7 @@ public class WeaponSelect : MonoBehaviour, IPointerEnterHandler/*, IPointerExitH
     [SerializeField] private GameObject wheel;
     [SerializeField] private Weapon type;
     [SerializeField] private Player player;
-    [SerializeField] private CurrentWeaponDisplay display;
+    [SerializeField] private GameObject display;
     public void OnPointerEnter(PointerEventData eventData) {
         if (player.LastWheel) {
             player.LastWheel.Deselect();
@@ -19,20 +19,18 @@ public class WeaponSelect : MonoBehaviour, IPointerEnterHandler/*, IPointerExitH
                 break;
             case Weapon.Sword:
                 player.Attack = new Slash(player);
-                display.SelectWeapon(Weapon.Sword);
                 break;
             case Weapon.Spear:
                 player.Attack = new Dash(player, 15f, 0.1f, 1f, 0.1f);
-                display.SelectWeapon(Weapon.Spear);
                 break;
             case Weapon.Shield:
                 player.Attack = new Guard(player, 0.5f, 2f, 0.2f, 3f, 15f);
-                display.SelectWeapon(Weapon.Shield);
                 break;
             default:
                 break;
         }
         wheel.transform.eulerAngles = new Vector3(0f, 0f, 120f * (int)type);
+        display.SetActive(true);
     }
 
     /*public void OnPointerExit(PointerEventData eventData) {
@@ -42,6 +40,7 @@ public class WeaponSelect : MonoBehaviour, IPointerEnterHandler/*, IPointerExitH
 
     public void Deselect() {
         GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.1f);
+        display.SetActive(false);
     }
 
     /*public void OnPointerClick(PointerEventData eventData) {
